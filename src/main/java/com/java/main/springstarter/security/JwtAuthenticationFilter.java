5f26d78d953f781d141b1c8f7974925ee44cad80
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 14:21:49 +0200

    (new): added JwtAuthenticationFilter

--- src/main/java/com/java/main/springstarter/security/JwtAuthenticationFilter.java
+++ src/main/java/com/java/main/springstarter/security/JwtAuthenticationFilter.java
@@ -1,7 +1,20 @@
 package com.java.main.springstarter.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
