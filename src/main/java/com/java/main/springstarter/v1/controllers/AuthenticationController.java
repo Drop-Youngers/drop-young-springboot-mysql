3ed5871c63a6489749566e2886a5c3f9664e3b28
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 23:19:27 +0200

    (new): added JwtAuthenticationResponse

--- src/main/java/com/java/main/springstarter/v1/controllers/AuthenticationController.java
+++ src/main/java/com/java/main/springstarter/v1/controllers/AuthenticationController.java
@@ -1,12 +1,17 @@
 package com.java.main.springstarter.v1.controllers;
 
+import com.java.main.springstarter.v1.payload.JwtAuthenticationResponse;
 import com.java.main.springstarter.v1.security.JwtTokenProvider;
 import com.java.main.springstarter.v1.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth")
