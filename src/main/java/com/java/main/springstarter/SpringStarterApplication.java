27f3cf0337043def62bca1f8e0fe2c91626fd9b1
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 11:29:23 +0200

    Initial commit

---
+++ src/main/java/com/java/main/springstarter/SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.java.main.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/main/springstarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.main.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
