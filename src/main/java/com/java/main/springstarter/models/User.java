14f4c1e5a886117cbcac71ed8fcf3486ed48c9bc
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 12:39:02 +0200

    (new): UserModel Done

--- src/main/java/com/java/main/springstarter/models/User.java
+++ src/main/java/com/java/main/springstarter/models/User.java
@@ -11,8 +11,9 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
+
+
 
 @Getter
 @Setter
