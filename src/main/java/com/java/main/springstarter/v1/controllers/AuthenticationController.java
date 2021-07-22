f265559446294333a58fec90bea2f78f25d357f9
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 23:03:29 +0200

    (new): added HELp.md

---
+++ src/main/java/com/java/main/springstarter/v1/controllers/AuthenticationController.java
@@ -0,0 +1,85 @@
+package com.java.main.springstarter.v1.controllers;
+
+import com.java.main.springstarter.v1.services.IUserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+public class AuthenticationController {
+    private IUserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Autowired
+    private  UserRepository userRepository;
+
+    @Autowired
+    private MailService mailService;
+
+
+
+    @PostMapping(path = "/signin")
+    @ApiOperation(value = "signin into your account")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@Valid @RequestBody SignInDTO signInRequest){
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        signInRequest.getEmail(),
+                        signInRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = null;
+
+        try{
+            jwt = jwtTokenProvider.generateToken(authentication);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @PostMapping(path = "/initiate-reset-password")
+    @ApiOperation(value = "Reset account password")
+    public ResponseEntity<ApiResponse> resetPassword(@RequestBody @Valid UserActiationDTO resetPasswordDTO){
+        User user = userRepository.findByEmail(resetPasswordDTO.getEmaill()).orElseThrow(() -> new AppException("user with this email "+resetPasswordDTO.getEmaill()+" is not found"));
+
+        user.setActivationCode(Utility.randomUUID(6,0,'N'));
+        user.setStatus(EUserStatus.RESET);
+
+        userRepository.save(user);
+
+        mailService.sendResetPasswordMail(user.getEmail(),user.getFirstName()+" "+user.getLastName(),user.getActivationCode());
+
+        return ResponseEntity.ok(new ApiResponse(true, "Successfully registered, please check your mail and activate account"));
+    }
+
+
+    @PostMapping(path = "/reset-password")
+    @ApiOperation(value = "confirm your password")
+    public ResponseEntity<ApiResponse> confirmPassword(@RequestBody @Valid ResetPasswordDTO resetPasswordDTO){
+        User user = userRepository.findByEmail(resetPasswordDTO.getEmail()).orElseThrow(() -> new EntityNotFoundException(User.class,"email",resetPasswordDTO.getEmail()));
+        if(Utility.isCodeValid(user.getActivationCode(),resetPasswordDTO.getActivationCode()) && (user.getStatus().equals(EUserStatus.RESET)) || user.getStatus().equals(EUserStatus.PENDING)){
+            user.setPassword(bCryptPasswordEncoder.encode(resetPasswordDTO.getPassword()));
+            user.setActivationCode(Utility.randomUUID(6, 0, 'N'));
+            user.setStatus(EUserStatus.ACTIVE);
+            userRepository.save(user);
+        }else{
+            throw new AppException("Invalid code or account has status of pending");
+        }
+        return ResponseEntity.ok(new ApiResponse(true,"user successfully activated"));
+    }
