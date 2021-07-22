eab8fac564e1c98b08b015ed900b228b12a48e92
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 14:14:19 +0200

    (new): added

--- src/main/java/com/java/main/springstarter/config/WebSecurity.java
+++ src/main/java/com/java/main/springstarter/config/WebSecurity.java
@@ -2,6 +2,7 @@ package com.java.main.springstarter.config;
 
 import com.java.main.springstarter.security.CustomUserDetailsService;
 import com.java.main.springstarter.security.JwtAuthenticationEntryPoint;
+import com.java.main.springstarter.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
