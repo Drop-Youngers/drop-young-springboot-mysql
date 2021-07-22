2792420255f2a819d2497b0a6714fe6d4a817b50
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 21:44:52 +0200

    (new): Added jaxb-api<

--- src/main/java/com/java/main/springstarter/v1/controllers/UserController.java
+++ src/main/java/com/java/main/springstarter/v1/controllers/UserController.java
@@ -15,7 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
@@ -48,73 +50,14 @@ public class UserController {
     ) {
         Pageable pageable = (Pageable) PageRequest.of(page, limit, Sort.Direction.ASC, "id");
         return userService.getAll(pageable);
-
+    }
 
     @GetMapping(path="/{id}")
     public ResponseEntity<User> getById(@PathVariable(value = "id") UUID id) {
-        return ResponseEntity.ok(this.institutionService.getById(id));
-    }
-
-    @PutMapping(path = "/{userID}")
-    public ResponseEntity<ApiResponse> updateUser(@PathVariable("userID") UUID userID,@Valid @RequestBody UserUpdateDTO userdataRequest){
-
-        userService.updateUser(userID,userdataRequest);
-
-        User user = userService.getUser(userID);
-
-        URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/users/{email}")
-                .buildAndExpand(user.getEmail()).toUri();
-        return ResponseEntity.created(location).body(new ApiResponse(true,"user  updated successfully",user));
-    }
-
-    @PutMapping(path = "/status/{userId}")
-    public ResponseEntity<ApiResponse> updateUserStatus(@PathVariable UUID userId, @RequestBody UpdateStatusDTO statusDTO){
-        User user = userService.updateUserStatus(userId,statusDTO.getStatus());
-        URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/users/{userID}")
-                .buildAndExpand(user.getId()).toUri();
-        return ResponseEntity.created(location).body(new ApiResponse(true,"user  updated successfully",user));
-    }
-
-
-    @DeleteMapping(path = "/{userID}")
-    public ResponseEntity<ApiResponse> deleteUser(@ApiParam(value="delete user",required = true) @PathVariable("userID") UUID userID){
-        return ResponseEntity.ok(new ApiResponse(true,"user removed successfully",userService.deleteUser(userID)));
-    }
-
-    @GetMapping(path = "/role/{roleName}")
-    public ResponseEntity<ApiResponse> getAllByRole(@RequestParam(value = "page", defaultValue = Constants.DEFAULT_PAGE_NUMBER) int page,
-                                                    @RequestParam(value = "size", defaultValue = Constants.DEFAULT_PAGE_SIZE) int size,
-                                                    @PathVariable ERoleType roleName){
-        return ResponseEntity.ok(new ApiResponse(true,"User with role "+roleName+" found ",userService.findAllByRole(page,size,roleName)));
-    }
-
-    @PostMapping(path = "/profile/{userID}")
-    public ResponseEntity<ApiResponse> updateProfileImage(@PathVariable UUID userID, @RequestParam("file") MultipartFile file){
-        User user = userService.updateProfileImage(userID,file);
-        return ResponseEntity.ok(new ApiResponse(true,"created",user));
-    }
-
-    @GetMapping(path = "/load-file/profile/{userID}")
-    public ResponseEntity<InputStreamResource> loadProfileImage(@PathVariable UUID userID) throws IOException {
-        User user = userService.getUser(userID);
-
-        if(user.getImageUrl() == null){
-            throw new AppException("Profile haven't been uploaded");
-        }
-
-        Path filePath = Paths.get(user.getImageUrl());
-        return ResponseEntity.ok().contentLength(Files.size(filePath))
-                .contentType(MediaType.parseMediaType(URLConnection.guessContentTypeFromName(filePath.toString())))
-                .body(new InputStreamResource(Files.newInputStream(filePath, StandardOpenOption.READ)));
-    }
-
-    @GetMapping(path = "/search/{searchKey}")
-    public List<User> searchUsers(@PathVariable String searchKey){
-        return userService.searchUser(searchKey);
+        return ResponseEntity.ok(this.userService.getById(id));
     }
 
     @PostMapping(path = "/register")
-    @ApiOperation(value = "create new user", response = User.class)
     public ResponseEntity<Response> register(@RequestBody @Valid SignUpDTO signUpRequest){
 
         User user = new User(signUpRequest.getEmail(),signUpRequest.getFirstName(),signUpRequest.getLastName(),signUpRequest.getMobile(),signUpRequest.getNationalId(),signUpRequest.getGender(),signUpRequest.getPassword());
