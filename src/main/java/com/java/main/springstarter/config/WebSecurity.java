95994892952459411a460075a7c61c43a7112f29
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 14:15:32 +0200

    (new): added

--- src/main/java/com/java/main/springstarter/config/WebSecurity.java
+++ src/main/java/com/java/main/springstarter/config/WebSecurity.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
