92d94fbfca90100a47140a839c82d36b069f90d6
DivinIrakiza <irakizadivin@gmail.com>
2021-07-22 11:56:14 +0200

    (new): TimestampAudit done

--- src/main/java/com/java/main/springstarter/audits/TimestampAudit.java
+++ src/main/java/com/java/main/springstarter/audits/TimestampAudit.java
@@ -1,10 +1,24 @@
 package com.java.main.springstarter.audits;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public abstract class TimestampAudit implements Serializable {
     private static final long serialVersionUID = 1L;
 
 
+    @CreationTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
 
+    @UpdateTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @Column(name="created_at")
+    private LocalDateTime updatedAt;
 }
